545389dc5601f89fe243275469775ca67e5da260
Петр Миронюк <stud0000210202@study.utmn.ru>
2022-05-17 20:21:50 +0500

    Почистил код

--- Tolik-Revival/Form1.cs
+++ Tolik-Revival/Form1.cs
@@ -12,26 +12,35 @@ namespace Tolik_Revival
 {
     public partial class GameForm : Form
     {
-
         public GameForm()
         {
             InitializeComponent();
         }
 
         bool right, left, up;
-        int speed = 20;
-        int force = 20;
-        int upCounter = 0;
-        int jump = 5;
-        int walkspeed = 20;
-        int score = 0;
         bool direct = true;
-        int picCounter = 1;
+
+        int attractionForce = 20;
+
+        int upCounter = 0;
+        int upMax = 5;
+        int upSpeed = 20;
+
+        int platformCounter = 0;
+        int platformMax = 20;
+
+        int score = 0;
+        int walkspeed = 20;
+
+        int enemyProbability = 50;
+
         Random rnd = new Random();
 
-        void CreatBullet()
+
+
+        void NewBullet()
         {
-            string tag = (direct)?"bulletR":"bulletL";
+            string tag = (direct) ? "0|30" : "0|-30";
 
             PictureBox bullet = new PictureBox
             {
@@ -39,141 +48,157 @@ namespace Tolik_Revival
                 Top = player.Top,
                 Left = player.Left,
                 Image = Properties.Resources.bullet,
-                Tag = tag
-
+                Tag = tag,
+                Name = "bullet"
             };
             this.Controls.Add(bullet);
         }
 
-        void CreatEnemy(int Top,int left)
+        void NewEnemy(int[] coord)
         {
-            PictureBox enemy1 = new PictureBox
+            if (rnd.Next(0, 100) > enemyProbability)
             {
-                Size = new Size(70, 70),
-                Top=Top-70,
-                Left=left,
-                Image = Properties.Resources.tolik_terr,
-                Tag="Enemy"
+                PictureBox enemy = new PictureBox
+                {
+                    Size = new Size(70, 70),
+                    Top = coord[0] - 70,
+                    Left = coord[1],
+                    Image = Properties.Resources.tolik_terr,
+                    Tag = "",
+                    Name = "enemy",
+                    SizeMode = PictureBoxSizeMode.CenterImage
+                };
+                this.Controls.Add(enemy);
+            }
+        }
+
+        void NewPlatform(int[] coord)
+        {
+            PictureBox platform = new PictureBox
+            {
+                Size = new Size(rnd.Next(100, 300), 30),
+                Top = coord[0],
+                Left = coord[1],
+                Image = Properties.Resources.ground,
+                Tag = "base",
+                Name = "platform",
             };
-            this.Controls.Add(enemy1);
+            this.Controls.Add(platform);
         }
 
-        void Base_move()
+        void CreateElements()
         {
-            if (player.Bounds.IntersectsWith(ground.Bounds))
+            if (platformCounter < platformMax)
             {
-                player.Top = ground.Top - player.Height;
-                force = 0;
-            }
-            
-
-            foreach (Control x in this.Controls)
-            {
-
-            
-                int rnd_Top = 623 - rnd.Next(1, 4) * 150;
-                int rnd_Left = rnd.Next(1200, 3000);
-
-
-                if (x is PictureBox && x.Tag == "base")
-                {
-                    if (player.Bounds.IntersectsWith(x.Bounds))
-                    {
-                        player.Top = x.Top - player.Height;
-                    }
-
-                    if (right == true && player.Left > 800)
-                    {
-                        x.Left -= 15;
-
-                        if (x.Left < -200)
-                        {
-
-                            x.Top = rnd_Top;
-                            x.Left = rnd_Left;
-                            x.Width = rnd.Next(100, 300);
-
-                            if (rnd.Next(0, 10)>5)
-                            {
-                                CreatEnemy(rnd_Top, rnd_Left);
-                            }
-
-                        }
-
-                    }
-                }
-                if (x is PictureBox && x.Tag == "Enemy")
-                {
-                    if (right == true && player.Left > 800)
-                    {
-                        x.Left -= 15;
-                    }
-                    if (x.Left < -100)
-                    {
-                        x.Dispose();
-                    }
-                }
-                if (x is PictureBox && (x.Tag == "bulletR" || x.Tag=="bulletL"))
-                {
-                    if (x.Left>0 && x.Left<1200)
-                    {
-                        if (x.Tag == "bulletR") x.Left += 30;
-
-                        if (x.Tag == "bulletL")  x.Left -= 30;
-
-                    }
-                    else
-                    {
-                        x.Dispose();
-                    }
-
-                    foreach (Control y in this.Controls)
-                    {
-                        if (y is PictureBox && y.Tag == "Enemy")
-                        {
-                            if (x.Bounds.IntersectsWith(y.Bounds))
-                            {
-                                x.Dispose();
-                                y.Dispose();
-                                score += 5;
-                                Score.Text = "Score: " + score.ToString();
-
-                            }
-
-                        }
-                    }
-                }
+                int[] coord = { 623 - rnd.Next(1, 4) * 150, rnd.Next(1200, 3000) };
+                NewPlatform(coord);
+                NewEnemy(coord);
+                platformCounter++;
             }
         }
 
-        void Player_move()
-        {
-            if (right == true) if (player.Left < 900) player.Left += walkspeed;
-            if (left == true) if (player.Left > 20) player.Left -= walkspeed;
 
-            if (up == true && upCounter < jump)
+        void MovePlayer()
+        {
+            if (right == true && player.Left < 900) player.Left += walkspeed;
+            if (left == true && player.Left > 20) player.Left -= walkspeed;
+
+            if (up == true && upCounter < upMax)
             {
-                if (player.Top > 50)
-                {
-                    player.Top -= speed;
-                    force = 50;
-                }
+                player.Top -= upSpeed;
+                attractionForce = 50;
                 upCounter++;
             }
-            if (upCounter == jump) up = false;
-            if (up == false) player.Top += force;
+            if (upCounter == upMax) up = false;
+            if (up == false) player.Top += attractionForce;
         }
+
+        void MovePlatform(Control x)
+        {
+            if (player.Bounds.IntersectsWith(x.Bounds))
+            {
+                player.Top = x.Top - player.Height;
+                upCounter = 0;
+            }
+
+            if (right == true && player.Left > 800)
+            {
+                x.Left -= walkspeed;
+            }
+
+            if (x.Left < -200)
+            {
+                x.Dispose();
+                platformCounter--;
+            }
+        }
+
+        void MoveEnemy(Control x)
+        {
+            if (right == true && player.Left > 800)
+            {
+                x.Left -= walkspeed;
+            }
+            if (x.Left < -100)
+            {
+                x.Dispose();
+            }
+        }
+
+        void MoveBullet(Control x)
+        {
+            if (x.Left > 0 && x.Left < 1200)
+            {
+                int[] coord = x.Tag.ToString().Split('|').Select(x => int.Parse(x)).ToArray();
+                x.Top += coord[0];
+                x.Left += coord[1];
+            }
+            else
+            {
+                x.Dispose();
+            }
+
+            KillEnemy(x);
+
+        }
+
+
+        void KillEnemy(Control x)
+        {
+            foreach (Control y in this.Controls)
+            {
+                if (y is PictureBox && y.Name == "enemy")
+                {
+                    if (x.Bounds.IntersectsWith(y.Bounds))
+                    {
+                        x.Dispose();
+                        y.Dispose();
+                        score += 5;
+                        Score.Text = "Score: " + score.ToString();
+                    }
+                }
+            }
+        }
+
+        void ElementProcessing()
+        {
+            CreateElements();
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if (x.Name == "platform") MovePlatform(x);
+                    if (x.Name == "enemy") MoveEnemy(x);
+                    if (x.Name == "bullet") MoveBullet(x);
+                }
+            }
+        }
+
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Player_move();
-            Base_move();
-            enemy_Bullets();
-        }
-
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            Bullets();
+            MovePlayer();
+            ElementProcessing();
         }
 
         private void GameForm_KeyDown(object sender, KeyEventArgs e)
@@ -181,21 +206,15 @@ namespace Tolik_Revival
             switch (e.KeyCode)
             {
                 case Keys.Right:
-                    
                     right = true;
                     direct = true;
-                    if (picCounter < 0) picCounter = 0;
-                    picCounter++;
-                    if (picCounter==1) player.Image = Properties.Resources.Tolik;
+                    player.Image = Properties.Resources.Tolik;
 
                     break;
                 case Keys.Left:
                     left = true;
-                    player.Image = Properties.Resources.TolikBack;
                     direct = false;
-                    if (picCounter > 0) picCounter = 0;
-                    picCounter--;
-                    if (picCounter == -1) player.Image = Properties.Resources.TolikBack;
+                    player.Image = Properties.Resources.TolikBack;
                     break;
                 case Keys.Up:
                     up = true;
@@ -215,10 +234,9 @@ namespace Tolik_Revival
                     break;
                 case Keys.Up:
                     up = false;
-                    upCounter = 0;
                     break;
                 case Keys.Space:
-                    CreatBullet();
+                    NewBullet();
                     break;
             }
         }
